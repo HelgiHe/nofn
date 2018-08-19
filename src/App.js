0ee901925678810eacd83814e987dea112118c8a
Helgi Helgason <HelgiHel@gmail.com>
2018-08-19 22:47:55 +0000

    inital

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>My React App!</h1>
+      </div>
+    );
+  }
+}
+export default App;
