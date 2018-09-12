bb1ff4e375073b1bc302f8be7e27950006028434
Helgi Helgason <HelgiHel@gmail.com>
2018-09-12 09:34:10 +0000

    scrolltotop

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import Home from './views/Home';
 import Login from './views/Login';
 import Input from './components/input';
 import NameList from './components/nameList';
+import { firebaseApiKey } from '../config/keys';
+import firebase from 'firebase/app';
 import './App.scss';
 
 class App extends Component {
+  componentDidMount = () => {
+    // Initialize Firebase
+    const config = {
+      apiKey: firebaseApiKey,
+      authDomain: 'nofn-4cfa4.firebaseapp.com',
+      databaseURL: 'https://nofn-4cfa4.firebaseio.com',
+      projectId: 'nofn-4cfa4',
+      storageBucket: 'nofn-4cfa4.appspot.com',
+      messagingSenderId: '375606112602'
+    };
+    firebase.initializeApp(config);
+  };
   render() {
     return (
       <Router>
         <div className="app">
-          Hi there!
+          Nofn
           <Input />
           <NameList />
           {/* <Switch>
