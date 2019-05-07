400cfb4350149a1b36aad5578c622a348014fc19
Helgi Helgason <HelgiHel@gmail.com>
2019-05-07 15:20:11 +0000

    usec env varialble for key

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -1,12 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 let firebaseApiKey = '';
-if (!process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
   firebaseApiKey = import('../../config/keys').firebaseApiKey;
 }
 
 const config = {
-  apiKey: firebaseApiKey,
+  apiKey:
+    process.env.NODE_ENV === 'production'
+      ? process.env.firebaseApiKey
+      : firebaseApiKey,
   authDomain: 'nofn-4cfa4.firebaseapp.com',
   databaseURL: 'https://nofn-4cfa4.firebaseio.com',
   projectId: 'nofn-4cfa4',
